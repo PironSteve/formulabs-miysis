84bb1bce8d55d38570bec8ca34d2a6893dbcc756
Steve <steve.piron@gmail.com>
2019-10-16 09:36:43 +0200

    adding some comments

--- js/index.js
+++ js/index.js
@@ -1,41 +1,46 @@
 function swapTiles(cell1,cell2) {
-    var temp = document.getElementById(cell1).className;
+    let temp = document.getElementById(cell1).className;
     document.getElementById(cell1).className = document.getElementById(cell2).className;
     document.getElementById(cell2).className = temp;
   }
   
   function shuffle() {
-  for (var row=1;row<=3;row++) {
-     for (var column=1;column<=3;column++) {
-      var row2=Math.floor(Math.random()*3 + 1);
-      var column2=Math.floor(Math.random()*3 + 1);
-      swapTiles("cell"+row+column,"cell"+row2+column2);
+    //On utilise des boucles pour accéder à chaque cellule de la grille 3x3
+  for (let row=1;row<=3;row++) { // Chaque ligne de la grille 3x3
+     for (let column=1;column<=3;column++) { // Chaque colonne de la grille 3x3
+      let randomRow=Math.floor(Math.random()*3 + 1); // Choisis une ligne aléatoire entre les 3 possible
+      let randomColumn=Math.floor(Math.random()*3 + 1); // Choisis une colonne aléatoire entre les 3 possible
+      swapTiles("cell"+row+column,"cell"+randomRow+randomColumn); // Mélange et échange la place des différentes cellules
     } 
   } 
   }
   
   function clickTile(row,column) {
-    var cell = document.getElementById("cell"+row+column);
-    var tile = cell.className;
-    if (tile!="tile9") { 
+    let cell = document.getElementById("cell"+row+column);
+    let tile = cell.className;
+    if (tile!="tile9") {
+        // Check si la cellule blanche est sur la droite
          if (column<3) {
            if ( document.getElementById("cell"+row+(column+1)).className=="tile9") {
              swapTiles("cell"+row+column,"cell"+row+(column+1));
              return;
            }
          }
+         // Check si la cellule blanche est sur la gauche
          if (column>1) {
            if ( document.getElementById("cell"+row+(column-1)).className=="tile9") {
              swapTiles("cell"+row+column,"cell"+row+(column-1));
              return;
            }
          }
+         // Check si la cellule blanche est au dessus
          if (row>1) {
            if ( document.getElementById("cell"+(row-1)+column).className=="tile9") {
              swapTiles("cell"+row+column,"cell"+(row-1)+column);
              return;
            }
          }
+         // Check si la cellule blanche est en dessous
          if (row<3) {
            if ( document.getElementById("cell"+(row+1)+column).className=="tile9") {
              swapTiles("cell"+row+column,"cell"+(row+1)+column);
